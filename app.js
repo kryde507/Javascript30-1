ab0759c23993fba47abf2e93969dbeafe3a84484
kryde507 <92607528+kryde507@users.noreply.github.com>
2022-10-11 15:20:19 -0600

    Update app.js

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const sounds = document.querySelectorAll("audio[data-key]");
 document.onkeydown = function (e) {
     for (let sound of sounds) {
         if (e.code === sound.dataset.key) {
-            sound.currentTime = 0;
+            sound.currentTime = 0; //resets the audio to 0 so it plays without having to finish
             sound.play();
-            let box = document.querySelector(`div[data-key=${e.code}]`);
+            let box = document.querySelector(`div[data-key=${e.code}]`); //selects the pressed key box
             box.classList.add('playing');
         }
     }
